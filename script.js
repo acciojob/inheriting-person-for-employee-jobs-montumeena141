be9262510cb95c0697184512f30a72e5745b0690
Montu Meena <158208396+montumeena141@users.noreply.github.com>
2025-02-11 11:06:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
-// complete this js code
-function Person(name, age) {}
+// function Person(name, age) {}
 
-function Employee(name, age, jobTitle) {}
+// function Employee(name, age, jobTitle) {}
 
-// Do not change code below this line
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  }
+}
+
+class Employee extends Person {
+  constructor(name, age, jobTitle) {
+    super(name, age); // Call the parent class constructor
+    this.jobTitle = jobTitle;
+  }
+
+  jobGreet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+  }
+}
+const person = new Person("Alice", 25);
+person.greet();  
+
+const employee = new Employee("Bob", 30, "Manager");
+employee.jobGreet();  
+
+
 window.Person = Person;
 window.Employee = Employee;
